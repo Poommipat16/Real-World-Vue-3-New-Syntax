7e3e0d7e3026c039a75f46b160a4e2f99b23c7d0
Poommipat_ps <poommipat.ps@gmail.com>
2024-05-23 08:53:54 +0700

    Finished

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -8,10 +8,14 @@ defineProps({
 </script>
 
 <template>
-  <div class="event-card">
-    <h2>{{ event.title }}</h2>
-    <span>@{{ event.time }} on {{ event.date }}</span>
-  </div>
+  <!-- ให้ card แต่ละ card click แล้ว navigate ไปหน้า details โดยใช้ routerlink -->
+  <!-- bind "to" หา event-details route เพื่อเรียกใช้ root นั้นๆ และทำการส่งค่า id ไปด้วย -->
+  <RouterLink class="event-link" :to="{ name: 'event-details', params: { id: event.id } }">
+    <div class="event-card">
+      <h2>{{ event.title }}</h2>
+      <span>@{{ event.time }} on {{ event.date }}</span>
+    </div>
+  </RouterLink>
 </template>
 
 <style scoped>
@@ -22,8 +26,13 @@ defineProps({
   border: 1px solid #39495c;
   margin-bottom: 18px;
 }
+
 .event-card:hover {
   transform: scale(1.01);
   box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+.event-link{
+  color: #2c3e50;
+  text-decoration: none;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EventListView from '../views/EventListView.vue'
+import EventDetails from '../views/EventDetails.vue'
 import AboutView from '../views/AboutView.vue'
 
 const router = createRouter({
@@ -9,6 +10,13 @@ const router = createRouter({
       path: '/',
       name: 'event-list',
       component: EventListView,
+    },
+    {
+      path: '/event/:id',
+      name: 'event-details',
+      // กำหนด props เป็น true เพื่อรับค่า prop แล้วส่งค่าต่อไปให้ eventDetails 
+      props: true,
+      component: EventDetails,
     },
     {
       path: '/about',
